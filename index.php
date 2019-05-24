22b5a15d9557dd0d0f2a5b96f1b814e08078b270
alireza <easazade@gmail.com>
2019-05-24 23:21:10 +0430

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <style>
+            a:link, a:visited, a:hover, a:active {
+                color: white;
+                text-decoration: none;
+                font-family: 'sans-serif', 'cursive';
+                padding: 10px;
+                background-color: dodgerblue;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>list of pages that i've made for practice</h1>
+        <a href="tables.html">tables</a>
+    </body>
+</html>
+<?php
+//echo "this is a good day";
+
+function doSome() {
+}
+
+?>
+
