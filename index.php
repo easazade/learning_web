a8a813ce6a5e80eb4a2588c23230cadd44e490ac
alireza <easazade@gmail.com>
2019-05-26 23:55:43 +0430

    adding js , html entities

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
             a:link, a:visited, a:hover, a:active {
                 color: white;
@@ -15,6 +16,8 @@
         <a href="tables.html">tables</a>
         <a href="lists.html">lists</a>
         <a href="menu.html">menu</a>
+        <a href="js.html">js</a>
+        <a href="entities.php">entities</a>
     </body>
 </html>
 <?php
